062b6eb07086c83cafc1921ec88cde82b4dbb438
Andress Barajas <andress.barajas@gmail.com>
2024-10-10 16:18:00 -0700

    Add raylib Tetris example by Niisoks

---
+++ examples/dreamcast/raylib/tetris/src/constants/vmuIcons.h
@@ -0,0 +1,10 @@
+#pragma once
+
+extern const char *vmuI;
+extern const char *vmuO;
+extern const char *vmuT;
+extern const char *vmuL;
+extern const char *vmuJ;
+extern const char *vmuS;
+extern const char *vmuZ;
+extern const char *vmuNULL;
